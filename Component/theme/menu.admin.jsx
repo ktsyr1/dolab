d62e94407325c7989927bcf950b27aa22c58f014
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-07-31 12:33:59 +0300

    ات
    ش

--- Component/theme/menu.admin.jsx
+++ Component/theme/menu.admin.jsx
@@ -3,7 +3,7 @@ import Link from "lib/link"
 export default function MenuAdmin() {
     return (
         <div className="box col ui R-p w-12">
-            <b className="  p" >dashboard</b>
+            <b className="  p"  >dashboard</b>
 
             {menu.map(a => {
                 return (
