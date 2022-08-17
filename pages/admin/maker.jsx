b9f4ded678a1cf1d4a8e8bde9a11fda55386ae5d
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-08-17 20:45:55 +0300

    Update maker.jsx

--- pages/admin/maker.jsx
+++ pages/admin/maker.jsx
@@ -6,7 +6,7 @@ import { Component, useContext, useState } from "react";
 import Forms, { Input } from "/theme/forms";
 import { AddCircleOutline, RemoveCircleOutline, PulseOutline, TrashOutline, PencilOutline } from "react-ionicons";
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter, withRouter } from "next/router";
 // import Table, { TableRow } from "../../theme/tables";
 import { Popconfirm, Table, Tag } from 'antd';
 import 'antd/dist/antd.css'
@@ -15,22 +15,21 @@ export default function Maker() {
     let lang = LangContext(locale);
     let [data, setData] = useState(ArrayMaker);
     // one 
+    let [id, setId] = useState(data.length);
     let [one, setOne] = useState({ name: '', models: [{ model: "", versions: [], id }] });
     function DeleteOne(e) {
         let id = e.id;
         let result = data.filter(item => item.id != id);
         setData(result);
     }
-    let [id, setId] = useState(data.length);
     function EditOne(e) {
         console.log(e);
+        setOne(e);
     }
     const columns = [
-        { title: lang.name, dataIndex: 'name', key: 'name', },
-        { title: lang.model, dataIndex: 'model', key: 'model', },
-        { title: lang.versions, dataIndex: 'versions', key: 'versions', },
+        { title: lang.name, dataIndex: 'name', editable: true },
         {
-            title: '', dataIndex: '', key: 'x',
+            title: '', dataIndex: '',
             render: (_, record) => data.length >= 1 ? (
                 <div className="box row" >
                     <div style={{ margin: '0 10px' }} onClick={() => EditOne(record)}>
@@ -43,8 +42,17 @@ export default function Maker() {
             ) : null, // 
         },
     ];
+    const expandedRowRender = (e) => {
+        const columns = [
+            { title: lang.model, dataIndex: 'model', key: 'model', },
+            { title: lang.versions, dataIndex: 'versions', key: 'versions', },
+        ];
+
+        return <Table columns={columns} dataSource={e.models} pagination={false} />;
+    };
     function send(res) {
         res['id'] = id + 1;
+        res['key'] = id + 1;
         setData([...data, res])
         setId(id + 1)
     }
@@ -54,37 +62,43 @@ export default function Maker() {
                 <title>{lang?.maker}</title>
             </Head>
             <Title title={lang?.maker} ui />
-            <Form onSubmit={send} />
-            <div className="m">
+            <Form onSubmit={send} data={one} />
+            <div className="m  ">
                 <Table
                     dataSource={data}
                     columns={columns}
+                    expandable={{
+                        expandedRowRender,
+                        defaultExpandedRowKeys: ['0'],
+                    }}
                     pagination={false}
+                    bordered
+
                 />
             </div>
         </div>
     );
 }
-function Form({ onSubmit }) {
+function Form({ onSubmit, data }) {
     let { locale } = useRouter();
     let lang = LangContext(locale);
     let [id, setId] = useState(0);
-    let [models, setModels] = useState([{ model: "", versions: [], id }]);
+    let [models, setModels] = useState(data.models);
     // set name
-    let [name, setName] = useState("");
-
+    let [name, setName] = useState(data.name);
+    console.log({ models, data });
     // loop ModelsCount
     let Models = () => models.map((model, i) => {
+        let { id, versions } = model
         // remove item in models
         function remove() {
-            let datas = [...models];
-            datas.splice(i, 1);
+            let datas = models.filter(item => item.id != id);
             setModels(datas);
         }
         function update(e) {
             // update model
-            let dataOthers = models.filter((item) => item.id != model.id);
-            let dataOne = models.filter(item => item.id === model.id)[0]
+            let dataOthers = models.filter((item) => item.id != id);
+            let dataOne = models.filter(item => item.id === id)[0]
             let name = e.target.name;
             let value = e.target.value;
 
@@ -100,7 +114,7 @@ function Form({ onSubmit }) {
                     : ''
                 }
                 <Input name="model" onChange={update} defaultValue={model.model} />
-                <Input name="versions" title={lang.version} onChange={update} defaultValue={model.versions?.join(',')} />
+                <Input name="versions" title={lang.version} onChange={update} defaultValue={versions?.join(',')} />
             </div>
         )
     })
@@ -117,7 +131,7 @@ function Form({ onSubmit }) {
 
     return (
         <div className="box col ui">
-            <Input name='name' placeholder={lang.name} onChange={e => setName(e.target.value)} className='w-10' />
+            <Input name='name' placeholder={lang.name} onChange={e => setName(e.target.value)} className='w-10' defaultValue={name} />
             <Models />
 
             <div className="box row alignX-full ">
@@ -130,16 +144,13 @@ function Form({ onSubmit }) {
         </div>
     )
 }
+// create class from  
 // critical thinking
 let ArrayMaker = [
     {
         id: 1,
-        name: "asgf",
-        models: [{ model: "wq", versions: ["v1"] }],
-    },
-    {
-        id: 2,
-        name: "werre we ree",
+        key: 1,
+        name: "test 1",
         models: [
             { model: "sdda", versions: ["v1", 'v2', 'v3'] },
             { model: "sad", versions: ["v1", 'v2'] },
@@ -147,3 +158,72 @@ let ArrayMaker = [
     },
 
 ]
+class Form2 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            _id: 0,
+            name: this.props.name,
+            models: this.props.models
+        }
+        this.Add = this.Add.bind(this)
+        this.New = this.New.bind(this)
+    }
+    New() { this.setState({ models: [...this.state.models, { model: "", versions: [], id: this.state._id + 1 }] }) }
+    Add() { this.props.onSubmit(this.state) }
+    // function Form3({ onSubmit, data }) {
+    // let { locale } = useRouter();
+    // let lang = LangContext(locale);   
+
+    Models() {
+        return models.map((model, i) => {
+            let { id, versions } = model
+            // remove item in models
+            function remove() {
+                let datas = models.filter(item => item.id != id);
+                this.setState({ models: datas });
+            }
+            function update(e) {
+                // update model
+                let dataOthers = models.filter((item) => item.id != id);
+                let dataOne = models.filter(item => item.id === id)[0]
+                let name = e.target.name;
+                let value = e.target.value;
+
+                dataOne[name] = name === 'versions' ? value.split(',') : value;
+                // dataOthers.push(dataOne)  
+                this.setState({ models: [...dataOthers, dataOne] });
+
+            }
+            return (
+                <div className="box row alignY " key={i} >
+                    {i > 0
+                        ? <RemoveCircleOutline title={'Plus'} color={'#00000'} height="40px" width="40px" onClick={remove} />
+                        : ''
+                    }
+                    <Input name="model" onChange={update} defaultValue={model.model} />
+                    <Input name="versions" title={lang.version} onChange={update} defaultValue={versions?.join(',')} />
+                </div>
+            )
+        })
+    }
+    render() {
+        console.log(this);
+        let lang = LangContext(this.props.locale);
+        return (
+            <div className="box col ui" >
+                <Input name='name' placeholder={lang.name} onChange={e => this.setState({ name: e.target.value })} className='w-10' defaultValue={this.state.name} />
+                {this.Models}
+
+                <div className="box row alignX-full ">
+                    <div className="box row alignY ui " onClick={this.New} style={{ cursor: 'pointer', margin: '0 10px', padding: 0 }}>
+                        <AddCircleOutline title={'Plus'} color={'#00000'} height="40px" width="40px" />
+                        <p style={{ margin: '0 10px' }}>add new model</p>
+                    </div>
+                    <button className="btn w-8 alignX" onClick={this.Add}>{lang.add}</button>
+                </div>
+            </div>
+        )
+    }
+}
+withRouter(Form2);
