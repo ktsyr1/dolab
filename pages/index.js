2dd9d980671ba06092e8ba8d75a9e98389195d78
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2022-09-01 09:02:04 +0300

    update pages/index

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
                 <title>dolib app</title>
             </Head>
             <div className="box alignY alignX h-10 ui w-10  ">
-                content v day 8/25 
+                content v day 8/31 
             </div>
         </div>
     )
